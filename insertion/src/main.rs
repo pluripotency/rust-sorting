5596626746d9654f6bca156e1925bda34ab302fd
Masayoshi Kokuryo <masakoku@gmail.com>
2019-02-09 01:34:25 +0900

    insertion: tested generics

--- insertion/src/main.rs
+++ insertion/src/main.rs
@@ -1,9 +1,8 @@
-fn insertion_sort(array: &mut Vec<usize>){
-    let length = array.len();
-    for i in 1..length {
+fn insertion_sort<T: Ord + Copy, U: Fn(T, T)->bool>(array: &mut Vec<T>, comparator: U) {
+    for i in 1..array.len() {
         let num_to_insert = array[i];
-        let mut compare_index = i.clone();
-        while (compare_index > 0) && (array[compare_index-1] > num_to_insert) {
+        let mut compare_index = i;
+        while (compare_index > 0) && comparator(num_to_insert, array[compare_index-1]) {
             array[compare_index] = array[compare_index - 1];
             compare_index -= 1;
         }
@@ -13,6 +12,6 @@ fn insertion_sort(array: &mut Vec<usize>){
 
 fn main() {
     let mut v = vec![20, 12, 45, 19, 91, 55];
-    insertion_sort(&mut v);
+    insertion_sort(&mut v, |a,b| a < b);
     println!("{:?}", v);
 }
